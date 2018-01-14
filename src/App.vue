af30f8f07111b2904566570da0eaf9a3406ca32a
corey-roth <info@coreyaroth.com>
2018-01-13 19:45:06 -0500

    Dynamic list
    Added dynamic functionality.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <template>
       <div>
-        <todo-list></todo-list>
+        <todo-list v-bind:todos="todos"></todo-list>
       </div>
     </template>
   </div>
@@ -12,20 +12,43 @@
 import TodoList from './components/TodoList';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     TodoList,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'myriad-pro','Myriad Pro',-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2e2e2f;
+  margin-top: 4em;
 }
 </style>
